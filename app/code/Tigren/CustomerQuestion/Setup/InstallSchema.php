91f0dc8067a548d21a04f64484958cca8836258e
Thanh Lich <lichk2002@gmail.com>
2023-05-04 16:16:22 +0700

    update copyright

--- app/code/Tigren/CustomerQuestion/Setup/InstallSchema.php
+++ app/code/Tigren/CustomerQuestion/Setup/InstallSchema.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2023 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
 namespace Tigren\CustomerQuestion\Setup;
 
 use Magento\Framework\DB\Ddl\Table;
